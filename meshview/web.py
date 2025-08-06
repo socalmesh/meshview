a9514a023845d2426eb1905234b5e2bbd086c030
Pablo Revilla <pablorevilla@gmail.com>
2025-08-06 16:47:17 -0700

    add API code for /api/packets

--- meshview/web.py
+++ meshview/web.py
@@ -23,7 +23,7 @@ import re
 import traceback
 
 SEQ_REGEX = re.compile(r"seq \d+")
-SOFTWARE_RELEASE= "2.0.3.08-08-25"
+SOFTWARE_RELEASE= "2.0.3.08-06-25"
 CONFIG = config.CONFIG
 
 env = Environment(loader=PackageLoader("meshview"), autoescape=select_autoescape())
